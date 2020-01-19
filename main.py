fdf84599ece57741d60a80a2f11bfbc7bf03dc71
Garen <garen.guan@qq.com>
2020-01-19 23:28:06 +0800

    0.2.0版本，使用线程提升效率

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
 import AutoOperation as AO
+import threading
+import time
+import functools
+import logging
 
-__version__ = '0.1.0'
+FORMAT = '%(asctime)-15s\t%(levelname)s %(module)s %(funcName)s %(message)s'
+path = open('config').readline().strip('\n\r ')
+logging.basicConfig(format=FORMAT, level=20)
+logger = logging.getLogger(__name__)
+file_h = logging.FileHandler(path + 'log.log')
+file_h.setFormatter(logging.Formatter(FORMAT))
+logger.addHandler(file_h)
+
+__version__ = '0.2.0'
+
+
+# TODO 路径使用PATH模块
+# TODO 日志使用logging模块，抽象相应代码
+
+def timer(*args, **kwargs):
+    def _timer(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = fn(*args, **kwargs)
+            end = time.time() - start
+            print("Total seconds: {:.3f}".format(end))
+            return result
+
+        return wrapper
+
+    return _timer
 
 
 def main():
-    path = open('config').readline().strip('\n\r ')
+    # lock = threading.Lock()
+
     dv = AO.DeviceInfo('devices.csv')
-    for lst in dv.getinfolist():
+
+    @timer()
+    def oper(lst, path):
         do = AO.DeviceOper(lst, path)
         try:
             do.oper()
-            with open(do.wrpath + 'log.log', 'a+') as log:
-                log.write('Hostname: {}, IP: {}, 已成功完成所有命令\n'.format(do.hostname, do.ip))
+            logger.info('Hostname: {}, IP: {}, 已成功完成所有命令'.format(do.hostname, do.ip))
+
         except:
-            with open(do.wrpath + 'log.log', 'a+') as log:
-                log.write('Hostname: {}, IP: {}, 未完成指令，请检查\n'.format(do.hostname, do.ip))
+            logger.info('Hostname: {}, IP: {}, 未完成指令，请检查'.format(do.hostname, do.ip))
+
+    for lst in dv.getinfolist():
+        threading.Thread(target=oper, args=(lst, path)).start()
+        print(threading.active_count())
 
 
 if __name__ == '__main__':
